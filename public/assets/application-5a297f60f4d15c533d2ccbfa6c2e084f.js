ba8e03633ca01926f239910f2490d825faf4e431
MoldySponge <matthew.seagle@gmail.com>
2014-08-21 22:20:51 -0700

    more back button modifications

--- public/assets/application-5a297f60f4d15c533d2ccbfa6c2e084f.js
+++ public/assets/application-5a297f60f4d15c533d2ccbfa6c2e084f.js
@@ -10642,16 +10642,18 @@ $(document).ready(function () {
 		fromTopSecond = 15 + squareHeight + 10 + "px";
 		fromTopThird = 30 + (2*squareHeight) + "px";
 		
-		backButtonWidth = (width / 4);
-		backButtonHeight = (height / 4);
+		backButtonWidth = (width / 2);
+		backButtonHeight = (height / 2);
 
 		fontSize = "4em";
 		lineHeight = "1em";		
 
 		$("BODY").css("font-size",fontSize);
 		$("BODY").css("line-height", lineHeight);		
+		
+		
 
-		$(".back").css("top", (5 + squareHeight - backButtonHeight));
+		$(".back").css("top", (5 + squareHeight - backButtonHeight + "px"));
 		$(".back").css("left", toMiddle);
 
 		$(".text").css("left", fromLeft);
@@ -10663,6 +10665,8 @@ $(document).ready(function () {
 
 		$(".picture").css("width", (squareWidth + "px"));
 		$(".picture").css("height", (squareHeight + "px"));
+		$(".picture").css("font-size", ".5em");
+
 		$(".experience").css("width", (squareWidth + "px"));
 		$(".experience").css( "height", (squareHeight + "px"));
 
